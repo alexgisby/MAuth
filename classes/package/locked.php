32fde0e60c73438887601267683245712f6a200e
Alex Gisby <alex.gisby@gmail.com>
2010-11-07 21:43:40 +0000

    Demo for locked packages.

---
+++ classes/package/locked.php
@@ -0,0 +1,19 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * A locked down package that has a higher precedence than the default.
+ *
+ * @package 	MAuth
+ * @category  	Packages
+ * @author 		Alex Gisby
+ */
+
+class Package_Locked extends Package_Default
+{
+	public $precedence = 5;
+	public $rules = array(
+	
+		'post'	=> false,
+		
+	);
+}
